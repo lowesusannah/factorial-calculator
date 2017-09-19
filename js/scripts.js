a3e071e9df2dc4d454412c5d1c090dfd17c0e584
Rane Fields and Susannah Lowe <rane@>
2017-09-19 15:31:03 -0700

    Add error checking to user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 var calculateFactorial = function(number) {
+  if (isNaN(number)) {
+    return NaN;
+  }
+  if (number < 0) {
+    return null;
+  }
+
   var product = 1;
   for (var i = 1; i <= number; i++) {
     product *= i;
@@ -6,17 +13,24 @@ var calculateFactorial = function(number) {
   return product;
 };
 
-
-
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var userInput = $("#number").val();
+    var userInput = parseInt($("#number input").val());
     var output = calculateFactorial(userInput);
 
-    $("#output h1").text(output);
-    $("#output").show();
+    $("#number").removeClass("has-error");
+    $("#number").addClass("has-success");
+    $("#number .help-block").hide();
 
+    if(isNaN(output) || output === null) {
+      $("#number").addClass("has-error");
+      $("#number .help-block").show();
+    } else {
+      $("#number").addClass("has-success");
+      $("#output h1").text(output);
+      $("#output").show();
+    }
   });
 });
